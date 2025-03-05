9d1a9ec4c11740556e75dfe98932b52613b5b2fa
Prami369 <prameelapoojari96@gmail.com>
2025-03-05 17:02:14 +0530

    first push

---
+++ jenkAframe/src/main/java/com/crm/basetestutility/Basetest.java
@@ -0,0 +1,88 @@
+package com.crm.basetestutility;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import com.crm.generic.fileUtility.ExcelUtility;
+import com.crm.generic.fileUtility.PropFileutility;
+import com.crm.generic.webdriverUtility.JavaUtility;
+import com.crm.generic.webdriverUtility.UtilityclassObject;
+import com.crm.generic.webdriverUtility.WebdriverUtility;
+import com.crm.objectrepository.Homepage;
+import com.crm.objectrepository.Loginpage;
+
+public class Basetest {
+	public WebDriver driver = null;
+	public static WebDriver sdriver;
+	public ExcelUtility eutil = new ExcelUtility();
+	public JavaUtility jutil = new JavaUtility();
+	public PropFileutility putil = new PropFileutility();
+	public WebdriverUtility wutil = new WebdriverUtility();
+//	public static ExtentReports report;
+	
+
+
+	@BeforeMethod 
+	public void configBM() throws IOException {
+		String url = putil.getDataFromPropertyfile("url");
+		String username = putil.getDataFromPropertyfile("username");
+		String pwd = putil.getDataFromPropertyfile("password");
+		Loginpage lp = new Loginpage(driver);
+		lp.login(url, username, pwd);
+	}
+
+	@AfterMethod 
+	public void configAM() {
+		Homepage hp = new Homepage(driver);
+		hp.logout();
+	}
+	
+	@BeforeClass 
+	public void configBC( ) throws IOException {
+		String Browsername = putil.getDataFromPropertyfile("browser");
+
+		if (Browsername.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (Browsername.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}else if (Browsername.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
+		
+		driver.manage().window().maximize();	
+
+	}
+
+	@AfterClass
+	public void configAC() {
+		driver.quit();
+	}
+
+	@BeforeSuite 
+	public void configAS() throws SQLException {
+//		dutil.getconnectionToDB();
+		System.out.println("DB connection done");
+	
+	}
+	
+	@AfterSuite (groups={"regression" ,"smoke"},alwaysRun =true)
+	public void configBS() throws SQLException {	
+		System.out.println("DB connection closed");
+
+	}
+
+	
+}
---
+++ jenkAframe/src/test/java/com/crm/campaigntest/campaignTest.java
@@ -0,0 +1,18 @@
+package com.crm.campaigntest;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.crm.basetestutility.Basetest;
+import com.crm.objectrepository.Homepage;
+
+public class campaignTest extends Basetest{
+	
+
+	@Test
+	public void navigatetoCampaignPageTest() throws IOException {	
+		Homepage hp = new Homepage(driver);
+		hp.navigateTocampaignpage();
+		
+}}
